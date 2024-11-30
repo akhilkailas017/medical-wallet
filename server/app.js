9b9810b93e9cdb30b8abe79ee459680148f421e7
akhilkailas017 <akhilkailas2001@gmail.com>
2024-11-30 07:12:37 +0530

    made some small change

--- server/app.js
+++ server/app.js
@@ -2,11 +2,9 @@ const express = require("express");
 const { mongoose } = require("mongoose");
 const app = express();
 const cors = require("cors");
-// const routes = require("./routes/routes");
 const adminAuth=require("./routes/adminAuth")
 const doctorAuth=require('./routes/doctorAuth')
 const cookieParser =require('cookie-parser')
-// const adminRoutes = require("./routes/adminRoutes");
 require("dotenv").config();
 const PORT = process.env.port;
 const mongourl=process.env.mongourl;
@@ -22,11 +20,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-// app.use("/", routes);
 app.use("/", doctorAuth);
 app.use("/", adminAuth);
-// app.use("/admin", adminRoutes);
-
 
 
 app.listen(PORT, () => {
